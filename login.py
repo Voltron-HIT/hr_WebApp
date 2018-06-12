c3a1bedc4b1ee09cd5019694bfe161a74da63dc1
blessed-tawanda <blessedmahuni@gmail.com>
2018-06-12 23:02:00 +0200

    updated login

--- login.py
+++ login.py
@@ -10,15 +10,10 @@ app = Flask(__name__)
 #Cross-Site Request Forgery prevention
 app.config['SECRET_KEY'] = 'secret key'
 #global variables
-<<<<<<< HEAD
-salt = b'$2b$11$Za4hFNuzn3Rvw7gLnUVZCu'
-
-=======
 dbUsername = 'admin'
 cost = 11
 salt = b'$2b$11$Za4hFNuzn3Rvw7gLnUVZCu'
 dbPassword = b'$2b$11$Za4hFNuzn3Rvw7gLnUVZCuv7B3iGvalQ6nMUss0o7/9OcsoBDc/Hi'
->>>>>>> b405a37af2ad86a980d62d65dd4de9076dad6f48
 
 #Unrouted functions
 def login_required(f):
@@ -45,13 +40,6 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = bcrypt.hashpw(request.form['password'].encode('utf-8'), salt)
-<<<<<<< HEAD
-        user = db.Credentials.find({'username': username})
-        for i in user:
-            dbUsername = i['username']
-            dbPassword = bytes(i['password'].encode('utf-8'))
-=======
->>>>>>> b405a37af2ad86a980d62d65dd4de9076dad6f48
 
         if username != dbUsername or  bcrypt.hashpw(request.form['password'].encode('utf-8'), password) != dbPassword:
             error = 'Invalid Credentials. Please try again.'
